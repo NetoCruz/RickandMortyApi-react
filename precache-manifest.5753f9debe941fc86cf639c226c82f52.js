527a6669d2f1496f60c259ac43b7c6ee908fc409
unknown <galaxy.net0710@gmail.com>
2019-06-03 19:49:52 -0500

    Updates

---
+++ precache-manifest.5753f9debe941fc86cf639c226c82f52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0870439174af29557b5",
+    "url": "/RickandMortyApi-react/static/css/main.67f599a2.chunk.css"
+  },
+  {
+    "revision": "e0870439174af29557b5",
+    "url": "/RickandMortyApi-react/static/js/main.aa4070d8.chunk.js"
+  },
+  {
+    "revision": "ea38baafbbd090142015",
+    "url": "/RickandMortyApi-react/static/js/runtime~main.a81685db.js"
+  },
+  {
+    "revision": "9b469934e582721c6b04",
+    "url": "/RickandMortyApi-react/static/css/2.e54eab2d.chunk.css"
+  },
+  {
+    "revision": "9b469934e582721c6b04",
+    "url": "/RickandMortyApi-react/static/js/2.28ca3480.chunk.js"
+  },
+  {
+    "revision": "700ea584cc400f1846250967af0ad909",
+    "url": "/RickandMortyApi-react/static/media/rick.700ea584.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RickandMortyApi-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d94c075cf1806327c0c59feb0c720cd0",
+    "url": "/RickandMortyApi-react/index.html"
+  }
+];
